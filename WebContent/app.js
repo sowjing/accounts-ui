9c5f1f89772dcd243dfcf4b3c99b02519d9431b6
sowjing <sowjing>
2016-03-21 07:12:04 -0600

    contact us form added

--- WebContent/app.js
+++ WebContent/app.js
@@ -10,6 +10,12 @@ app.config(function($routeProvider) {
 	}).when('/customers', {
 		controller : custCtrl,
 		templateUrl : 'customers.html'
+	}).when('/stmt', {
+		controller : stmtCtrl,
+		templateUrl : 'statements.html'
+	}).when('/feedback', {
+		controller : fdCtrl,
+		templateUrl : 'feedback.html'
 	}).otherwise({
 		redirectTo : '/'
 	})
@@ -19,29 +25,41 @@ var custCtrl = function($scope, $location) {
 	$scope.test = "testing...hello";
 }
 
+var stmtCtrl = function($scope, $http) {
+
+	$scope.wait = true;
+
+	$http.get("http://localhost:8989/AccountMgmt/acctservice/txn/gettxn").then(
+			function(response) {
+				$scope.wait = false;
+				$scope.ready = true;
+				$scope.txn = response.data;
+			});
+}
+
 var homeCtrl = function($cookies, $scope, $location) {
 	var custName = $cookies.get('custName');
 	// Setting a name
-	//$cookies.put('custName', 'John');
-	
-	if(custName) {
+	// $cookies.put('custName', 'John');
+
+	if (custName) {
 		$scope.greeting = "Hello " + custName;
 	}
-	
+
 	$scope.test = "testing...hello";
 }
 
 var txnCtrl = function($scope, $http) {
 
-	$http.get("http://localhost:8989/AccountMgmt/acctservice/txn/gettxn").then(
-			function(response) {
-				$scope.txn = response.data;
-			});
-
 	$scope.addTxn = function() {
-		if (!$scope.desc || $scope.amt || $scope.crDb) {
+		if (!$scope.desc || !$scope.amt || !$scope.crDb) {
 
 			$scope.errortext = "Please fill all details";
+			return;
+		}
+		
+		if(! angular.isNumber($scope.amt)) {
+			$scope.errortext = "Amount is invalid. Please enter a valid number.";
 			return;
 		}
 
@@ -58,3 +76,13 @@ var txnCtrl = function($scope, $http) {
 	}
 }
 
+var fdCtrl = function($scope) {
+
+	$scope.fbshow = true;
+
+	$scope.sendFeedback = function() {
+
+		$scope.fbdone = true;
+		$scope.fbshow = false;
+	}
+}
